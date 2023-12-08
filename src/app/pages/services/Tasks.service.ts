ff7b07e950498a10190dec95204c470e916ad142
Fagbenro Oluwafikayomi Caleb <fagbenrocoa@gmail.com>
2023-12-08 23:18:23 +0100

    Started working with data

--- src/app/pages/services/Tasks.service.ts
+++ src/app/pages/services/Tasks.service.ts
@@ -1,13 +1,22 @@
 import { Injectable } from "@angular/core";
+import { Subject } from "rxjs";
 import { Task } from "src/app/shared/task.model";
 
 @Injectable({providedIn: 'root'})
 export class TasksService{
+    taskChange = new Subject<Task[]>()
+
     Tasks: Task[] = [
-        new Task('Task A', 'Description A', 'List 1', new Date(2023, 3, 4)),
-        new Task('Task B', 'Description B', 'List 2', new Date()),
-        new Task('Task C', 'Description C', 'List 3', new Date()),
-        new Task('Task D', 'Description D', 'List 4', new Date(2023, 3, 6)),
-        new Task('Task E', 'Description E', 'List 5', new Date(2023, 3, 6)),
+        new Task('Task A', 'Description A', 'personal', new Date(2023, 3, 4)),
+        new Task('Task B', 'Description B', 'work', new Date()),
+        new Task('Task C', 'Description C', 'personal', new Date()),
+        new Task('Task D', 'Description D', 'work', new Date(2023, 3, 6)),
+        new Task('Task E', 'Description E', 'personal', new Date(2023, 3, 6)),
     ]
+    getTasks() {
+        return this.Tasks
+    }
+    getTask(i: number) {
+        return this.Tasks[i]
+    }
 }
