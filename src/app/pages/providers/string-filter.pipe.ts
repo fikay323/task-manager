6ab0a1521ff226dace9b07ea57e57c9bc7b54511
Fagbenro Oluwafikayomi Caleb <fagbenrocoa@gmail.com>
2023-12-09 18:50:43 +0100

    String filter is complete

--- src/app/pages/providers/string-filter.pipe.ts
+++ src/app/pages/providers/string-filter.pipe.ts
@@ -1,4 +1,5 @@
 import { Pipe, PipeTransform } from '@angular/core';
+import { Task } from 'src/app/shared/task.model';
 
 @Pipe({
   name: 'stringFilter',
@@ -6,8 +7,16 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class StringFilterPipe implements PipeTransform {
 
-  transform(value: string, filter: string) {
-    return null;
+  transform(value: Task[], filter: string): Task[] {
+    let taskArray: Task[] = []
+    taskArray = value.filter((task) => {
+      return task.taskList === filter
+    })
+    taskArray.sort((a, b) => {
+      const date1 = a.taskDueDate
+      const date2 = b.taskDueDate
+      return date1.getTime() - date2.getTime()
+    })
+    return taskArray;
   }
-
 }
