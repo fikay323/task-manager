ef76adcec2d440c8594e12f8a9cb45139fdcfa70
Fagbenro Oluwafikayomi Caleb <fagbenrocoa@gmail.com>
2023-12-10 22:44:37 +0100

    Added default environment

---
+++ src/environments/environment-default.ts
@@ -0,0 +1,13 @@
+export const defaultEnvironment = {
+  firebase: {
+    apiKey: "AIzaSyA9SAqzk_9d6uMt364o1HJ3L7rejHsnsVg",
+    authDomain: "task-manager-c8110.firebaseapp.com",
+    databaseURL: "https://task-manager-c8110-default-rtdb.firebaseio.com",
+    projectId: "task-manager-c8110",
+    storageBucket: "task-manager-c8110.appspot.com",
+    messagingSenderId: "19729874909",
+    appId: "1:19729874909:web:9b99349e289a0c94428034",
+    measurementId: "G-9DHN1BVK0Y"
+  },
+  production: false
+};
