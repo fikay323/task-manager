4a9b2e339850ba4a54a5f7922731932f0c8eded8
Fagbenro Oluwafikayomi Caleb <fagbenrocoa@gmail.com>
2023-12-12 19:09:32 +0100

    Added responsiveness to the navbar

--- src/app/pages/pages.component.ts
+++ src/app/pages/pages.component.ts
@@ -1,4 +1,4 @@
-import { Component, Renderer2, ViewChild } from "@angular/core";
+import { Component, HostListener, Renderer2, ViewChild } from "@angular/core";
 
 @Component({
     selector: 'app-pages',
@@ -6,32 +6,25 @@ import { Component, Renderer2, ViewChild } from "@angular/core";
     styleUrls: ['./pages.component.css']
 })
 export class PagesComponent{
-    @ViewChild('sidebar') sideBar: any
-    taskDetailClosed: boolean = true
+  toggledSidebar = true
+  screenWidth: number
 
-    constructor(private renderer: Renderer2) {}
+  toggleSidebar() {
+    this.toggledSidebar = !this.toggledSidebar
+    console.log(this.toggledSidebar)
+  }
 
-    toggleSideBar() {
-        if(this.sideBar.nativeElement.clientWidth === 0){
-            const classesToAdd = ['p-3', 'w-[200px]', 'translate-x-0'];
-            const nativeElement = this.sideBar.nativeElement;
-            classesToAdd.forEach(className => {
-              this.renderer.addClass(nativeElement, className);
-            });
-            const classesToRemove = ['p-0', 'w-0', 'translate-x-[-200%]'];
-            classesToRemove.forEach(className => {
-              this.renderer.removeClass(nativeElement, className);
-            });
-        } else {
-            const classesToRemove = ['p-3', 'w-[200px]', 'translate-x-0', 'sm:w-[200px]', 'sm:translate-x-0', 'sm:p-3'];
-            const nativeElement = this.sideBar.nativeElement;
-            classesToRemove.forEach(className => {
-              this.renderer.removeClass(nativeElement, className);
-            });
-            const classesToAdd = ['p-0', 'w-0', 'translate-x-[-200%]'];
-            classesToAdd.forEach(className => {
-              this.renderer.addClass(nativeElement, className);
-            });
-        }
+  @HostListener('window:resize', ['$event'])
+  onResize(event: any): void {
+    this.screenWidth = window.innerWidth;
+    this.adjustBasedOnWidth();
+  }
+
+  adjustBasedOnWidth(): void {
+    if (this.screenWidth < 500) {
+      this.toggledSidebar = true
+    } else {
+      this.toggledSidebar = false
     }
+  }
 }
